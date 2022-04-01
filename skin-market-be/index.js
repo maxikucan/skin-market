fe304ad1b3a6f051e48d3d1bbbcb602d76750185
Maximiliano Kucan <Maximiliano.Kucan@endava.com>
2022-04-01 17:03:30 -0300

    refactor get users endpoint

--- skin-market-be/index.js
+++ skin-market-be/index.js
@@ -1,34 +1,14 @@
 const express = require('express');
-const { Pool } = require('pg');
+
 const app = express();
-let usersArr = [];
+
 
 app.use('/', require('./router/routes'));
+app.use('/userConnect', require('./router/routes'));
 
 app.listen(8080, () => {
     console.log('Server running on port 8080')
 })
 
-const config = {
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'skin_market_db'
-};
 
-const pool = new Pool(config);
 
-app.get('/userConnect', (req, res) => {
-    getUsers();
-    res.send(JSON.stringify(usersArr));
-})
-
-const getUsers = async () => {
-    try {
-        const res = await pool.query('SELECT * FROM users');
-        console.log(res.rows);
-        usersArr.push(res.rows);
-    } catch (e) {
-        console.log(e);
-    }
-}
--- skin-market-be/router/routes.js
+++ skin-market-be/router/routes.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const { Pool } = require('pg');
+
 const router = express.Router();
+let usersArr;
+
+const config = {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'skin_market_db'
+};
 
 router.get('/', (req, res) =>{
     res.send('Hello World. Backend is running');
 });
+
+const pool = new Pool(config);
+
+router.get('/userConnect', async (req, res) => {
+    const response = await getUsers();
+    res.send(JSON.stringify(response));
+})
+
+const getUsers = async () => {
+    try {
+        const res = await pool.query('SELECT * FROM users');
+        console.log(res.rows);
+        return res.rows;
+
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 module.exports = router;
